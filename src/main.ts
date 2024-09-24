5bebc13f664c9795244c2b58519ae01c1151fb4f
r-4bb1t <r4bb1t.github@gmail.com>
2024-09-24 18:10:19 +0900

    add korean prompt

--- src/main.ts
+++ src/main.ts
@@ -85,7 +85,7 @@ function createPrompt(file: File, chunk: Chunk, prDetails: PRDetails): string {
 - Provide comments and suggestions ONLY if there is something to improve, otherwise "reviews" should be an empty array.
 - Write the comment in GitHub Markdown format.
 - Use the given description only for the overall context and only comment the code.
-- Your comments should be in Korean.
+- IMPORTANT: You SHOULD use KOREAN language for the review. (한국어로 리뷰를 작성해주세요)
 - IMPORTANT: NEVER suggest adding comments to the code.
 
 Review the following code diff in the file "${
